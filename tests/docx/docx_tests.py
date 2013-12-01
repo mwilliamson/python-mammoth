b167af309185c04815c07271b7f8ed8db1d485db
Michael Williamson <mike@zwobble.org>
2013-12-01 20:37:02 +0000

    Open files in binary mode

--- tests/docx/docx_tests.py
+++ tests/docx/docx_tests.py
@@ -8,7 +8,7 @@ from ..testing import test_path
 class ReadTests(object):
     @istest
     def can_read_document_with_single_paragraph_with_single_run_of_text(self):
-        with open(test_path("single-paragraph.docx")) as fileobj:
+        with open(test_path("single-paragraph.docx"), "rb") as fileobj:
             result = docx.read(fileobj=fileobj)
             expected_document = documents.document([
                 documents.paragraph([
--- tests/mammoth_tests.py
+++ tests/mammoth_tests.py
@@ -7,7 +7,7 @@ import mammoth
 
 @istest
 def docx_containing_one_paragraph_is_converted_to_single_p_element():
-    with open(test_path("single-paragraph.docx")) as fileobj:
+    with open(test_path("single-paragraph.docx"), "rb") as fileobj:
         result = mammoth.convert_to_html(fileobj=fileobj)
         assert_equal("<p>Walking on imported air</p>", result.value)
         assert_equal([], result.messages)
