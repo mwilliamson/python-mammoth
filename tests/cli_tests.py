50cddcce1e3a0e14efaf66c22f63a273edd95ba4
Michael Williamson <mike@zwobble.org>
2013-12-17 19:56:00 +0000

    Handle unicode output properly in CLI

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -13,8 +13,8 @@ _local = spur.LocalShell()
 def html_is_printed_to_stdout_if_output_file_is_not_set():
     docx_path = test_path("single-paragraph.docx")
     result = _local.run(["mammoth", docx_path])
-    assert_equal("", result.stderr_output)
-    assert_equal("<p>Walking on imported air</p>", result.output)
+    assert_equal(b"", result.stderr_output)
+    assert_equal(b"<p>Walking on imported air</p>", result.output)
 
 
 @istest
@@ -23,8 +23,8 @@ def html_is_written_to_file_if_output_file_is_set():
         output_path = os.path.join(temp_dir.path, "output.html")
         docx_path = test_path("single-paragraph.docx")
         result = _local.run(["mammoth", docx_path, output_path])
-        assert_equal("", result.stderr_output)
-        assert_equal("", result.output)
+        assert_equal(b"", result.stderr_output)
+        assert_equal(b"", result.output)
         with open(output_path) as output_file:
             assert_equal("<p>Walking on imported air</p>", output_file.read())
     
