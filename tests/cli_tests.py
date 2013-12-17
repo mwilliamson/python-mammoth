dc7bff2e738e5259e51bbb612ffcbae00b5505e6
Michael Williamson <mike@zwobble.org>
2013-12-17 19:46:59 +0000

    Test writing to file from CLI

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -1,6 +1,8 @@
-from nose.tools import istest, assert_equal
+import os
 
+from nose.tools import istest, assert_equal
 import spur
+import tempman
 
 from .testing import test_path
 
@@ -13,3 +15,16 @@ def html_is_printed_to_stdout_if_output_file_is_not_set():
     result = _local.run(["mammoth", docx_path])
     assert_equal("", result.stderr_output)
     assert_equal("<p>Walking on imported air</p>", result.output)
+
+
+@istest
+def html_is_written_to_file_if_output_file_is_set():
+    with tempman.create_temp_dir() as temp_dir:
+        output_path = os.path.join(temp_dir.path, "output.html")
+        docx_path = test_path("single-paragraph.docx")
+        result = _local.run(["mammoth", docx_path, output_path])
+        assert_equal("", result.stderr_output)
+        assert_equal("", result.output)
+        with open(output_path) as output_file:
+            assert_equal("<p>Walking on imported air</p>", output_file.read())
+    
