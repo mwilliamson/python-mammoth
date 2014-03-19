04cae35fcaaffa42fea663865b6cb11c117d423b
Michael Williamson <mike@zwobble.org>
2014-03-19 10:16:42 +0000

    Assert image file output when using CLI

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -1,4 +1,5 @@
 import os
+import base64
 
 from nose.tools import istest, assert_equal
 import spur
@@ -29,21 +30,28 @@ def html_is_written_to_file_if_output_file_is_set():
             assert_equal("<p>Walking on imported air</p>", output_file.read())
 
 
+_image_base_64 = b"iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAOvgAADr4B6kKxwAAAABNJREFUKFNj/M+ADzDhlWUYqdIAQSwBE8U+X40AAAAASUVORK5CYII="
+
 @istest
 def inline_images_are_included_in_output_if_writing_to_single_file():
     docx_path = test_path("tiny-picture.docx")
     result = _local.run(["mammoth", docx_path])
-    assert_equal(b"""<p><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAOvgAADr4B6kKxwAAAABNJREFUKFNj/M+ADzDhlWUYqdIAQSwBE8U+X40AAAAASUVORK5CYII=" /></p>""", result.output)
+    assert_equal(b"""<p><img src="data:image/png;base64,""" + _image_base_64 + b"""" /></p>""", result.output)
 
 
 @istest
 def images_are_written_to_separate_files_if_output_dir_is_set():
     with tempman.create_temp_dir() as temp_dir:
         output_path = os.path.join(temp_dir.path, "tiny-picture.html")
+        image_path = os.path.join(temp_dir.path, "1.png")
+        
         docx_path = test_path("tiny-picture.docx")
         result = _local.run(["mammoth", docx_path, "--output-dir", temp_dir.path])
         assert_equal(b"", result.stderr_output)
         assert_equal(b"", result.output)
         with open(output_path) as output_file:
             assert_equal("""<p><img src="1.png" /></p>""", output_file.read())
+        
+        with open(image_path, "rb") as image_file:
+            assert_equal(_image_base_64, base64.b64encode(image_file.read()))
     
