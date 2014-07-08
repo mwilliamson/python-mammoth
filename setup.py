0bee7e30ca0f184169196e46a85e3a9d9c92eea0
Michael Williamson <mike@zwobble.org>
2014-07-08 11:18:06 +0100

    Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info[:2] <= (2, 6):
 
 setup(
     name='mammoth',
-    version='0.3.2',
+    version='0.3.3',
     description='Convert Word documents to simple and clean HTML',
     long_description=read("README"),
     author='Michael Williamson',
