7e3ef0ebd0c3453ce351af70ccdb25e93de1d7a9
Michael Williamson <mike@zwobble.org>
2016-11-24 21:55:13 +0000

    Add HTML path parser

--- mammoth/styles/parser/token_iterator.py
+++ mammoth/styles/parser/token_iterator.py
@@ -1,7 +1,12 @@
+# TODO: check indices
+
 class TokenIterator(object):
     def __init__(self, tokens):
         self._tokens = tokens
         self._index = 0
+    
+    def peek_token_type(self):
+        return self._tokens[self._index].type
     
     def next_value(self, token_type):
         return self._next(token_type).value
@@ -14,9 +19,9 @@ class TokenIterator(object):
         else:
             raise self._unexpected_token_type(token_type, token)
     
-    def skip(self, token_type, token_value):
+    def skip(self, token_type, token_value=None):
         token = self._tokens[self._index]
-        if token.type == token_type and token.value == token_value:
+        if token.type == token_type and (token_value is None or token.value == token_value):
             self._index += 1
             return True
         else:
@@ -29,3 +34,16 @@ class TokenIterator(object):
             return True
         else:
             return False
+    
+    def try_skip_many(self, tokens):
+        start = self._index
+        for token_type, token_value in tokens:
+            token = self._tokens[self._index]
+            if not (token.type == token_type and (token_value is None or token.value == token_value)):
+                self._index = start
+                return False
+            else:
+                self._index += 1
+        
+        return True
+                
