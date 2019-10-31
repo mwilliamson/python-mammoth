b936f99b15bde8ba645b685e8a8e5962e3528a7a
Michael Williamson <mike@zwobble.org>
2019-10-31 10:55:50 +0000

    Update Python support to 2.7 and 3.4 to 3.8

--- setup.py
+++ setup.py
@@ -8,13 +8,6 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-_install_requires = [
-    "cobble>=0.1.3,<0.2",
-]
-
-if sys.version_info[:2] <= (2, 6):
-    _install_requires.append("argparse>=1.1,<2.0")
-
 setup(
     name='mammoth',
     version='1.4.9',
@@ -30,22 +23,23 @@ setup(
         ]
     },
     keywords="docx word office clean html markdown md",
-    install_requires=_install_requires,
+    install_requires=[
+        "cobble>=0.1.3,<0.2",
+    ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
 
