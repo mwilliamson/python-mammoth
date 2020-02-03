6f7852e45a08c60bdb5961c7a43b1b5bc41d3b10
Michael Williamson <mike@zwobble.org>
2020-02-03 19:25:19 +0000

    Add license to setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "cobble>=0.1.3,<0.2",
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    license="BSD-2-Clause",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
