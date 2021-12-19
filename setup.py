48d3cc73906ccba5e45c5cf9ec091e07808b951d
Garth Smedley <garth.smedley@gmail.com>
2021-12-19 06:46:00 -0400

    Update version

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ def read(fname):
 
 setup(
     name='mammoth',
-    version='1.4.18',
+    version='1.4.18.1',
     description='Convert Word documents from docx to simple and clean HTML and Markdown',
-    long_description=read("README"),
+    long_description=read("README.md"),
     author='Michael Williamson',
     author_email='mike@zwobble.org',
-    url='http://github.com/mwilliamson/python-mammoth',
+    url='http://github.com/gsmedley/python-mammoth',
     packages=['mammoth', 'mammoth.docx', 'mammoth.html', 'mammoth.styles', 'mammoth.styles.parser', 'mammoth.writers'],
     entry_points={
         "console_scripts": [
