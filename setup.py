e69f93713e160ff80f12585dee83b9620482b9a0
Michael Williamson <mike@zwobble.org>
2022-11-25 23:01:15 +0000

    Explicitly support Python 3.10 and 3.11

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
 
