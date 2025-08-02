cbca9920da21f00110153175481472c56ec92436
Michael Williamson <mike@zwobble.org>
2025-08-02 16:39:44 +0100

    Bump version to 1.10.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='mammoth',
-    version='1.9.1',
+    version='1.10.0',
     description='Convert Word documents from docx to simple and clean HTML and Markdown',
     long_description=read("README"),
     author='Michael Williamson',
